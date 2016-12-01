e27982c37034e451006095116dd457372218628c
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2016-12-01 13:15:06 +0100

    No version information can be obtained using git -- use rpm.

--- src/applications/config/controller/PhabricatorConfigVersionController.php
+++ src/applications/config/controller/PhabricatorConfigVersionController.php
@@ -96,6 +96,14 @@ final class PhabricatorConfigVersionController
       'phutil',
     );
 
+    $pkg_specs = array(
+      'phabricator' => 'phabricator',
+      'arcanist' => 'arcanist',
+      'phutil' => 'libphutil',
+      'sprint' => 'phabricator-extension-sprint',
+      'security' => 'phabricator-extension-security',
+    );
+
     $all_libraries = PhutilBootloader::getInstance()->getAllLibraries();
     // This puts the core libraries at the top:
     $other_libraries = array_diff($all_libraries, $specs);
@@ -105,135 +113,22 @@ final class PhabricatorConfigVersionController
     $remote_futures = array();
 
     foreach ($specs as $lib) {
-      $root = dirname(phutil_get_library_root($lib));
+      if (array_key_exists($lib, $pkg_specs)) {
+        $lib = $pkg_specs[$lib];
+      }
 
-      $log_command = csprintf(
-        'git log --format=%s -n 1 --',
-        '%H %ct');
-
-      $remote_command = csprintf(
-        'git remote -v');
-
-      $log_futures[$lib] = id(new ExecFuture('%C', $log_command))
-        ->setCWD($root);
-
-      $remote_futures[$lib] = id(new ExecFuture('%C', $remote_command))
-        ->setCWD($root);
+      $results[$lib] =
+        id(new ExecFuture("rpm --query --queryformat=%%{VERSION} $lib"));
     }
 
-    $all_futures = array_merge($log_futures, $remote_futures);
+    foreach ($results as $key => $future) {
+        list($ret, $version) = $future->resolve();
 
-    id(new FutureIterator($all_futures))
-      ->resolveAll();
-
-    // A repository may have a bunch of remotes, but we're only going to look
-    // for remotes we host to try to figure out where this repository branched.
-    $upstream_pattern = '(github\.com/phacility/|secure\.phabricator\.com/)';
-
-    $upstream_futures = array();
-    $lib_upstreams = array();
-    foreach ($specs as $lib) {
-      $remote_future = $remote_futures[$lib];
-
-      list($err, $stdout) = $remote_future->resolve();
-      if ($err) {
-        // If this fails for whatever reason, just move on.
-        continue;
-      }
-
-      // These look like this, with a tab separating the first two fields:
-      // remote-name     http://remote.uri/ (push)
-
-      $upstreams = array();
-
-      $remotes = phutil_split_lines($stdout, false);
-      foreach ($remotes as $remote) {
-        $remote_pattern = '/^([^\t]+)\t([^ ]+) \(([^)]+)\)\z/';
-        $matches = null;
-        if (!preg_match($remote_pattern, $remote, $matches)) {
-          continue;
+        if ($ret > 0) {
+            $results[$key] = 'Unknown';
+        } else {
+            $results[$key] = $version;
         }
-
-        // Remote URIs are either "push" or "fetch": we only care about "fetch"
-        // URIs.
-        $type = $matches[3];
-        if ($type != 'fetch') {
-          continue;
-        }
-
-        $uri = $matches[2];
-        $is_upstream = preg_match($upstream_pattern, $uri);
-        if (!$is_upstream) {
-          continue;
-        }
-
-        $name = $matches[1];
-        $upstreams[$name] = $name;
-      }
-
-      // If we have several suitable upstreams, try to pick the one named
-      // "origin", if it exists. Otherwise, just pick the first one.
-      if (isset($upstreams['origin'])) {
-        $upstream = $upstreams['origin'];
-      } else if ($upstreams) {
-        $upstream = head($upstreams);
-      } else {
-        $upstream = null;
-      }
-
-      if (!$upstream) {
-        continue;
-      }
-
-      $lib_upstreams[$lib] = $upstream;
-
-      $merge_base_command = csprintf(
-        'git merge-base HEAD %s/master --',
-        $upstream);
-
-      $root = dirname(phutil_get_library_root($lib));
-
-      $upstream_futures[$lib] = id(new ExecFuture('%C', $merge_base_command))
-        ->setCWD($root);
-    }
-
-    if ($upstream_futures) {
-      id(new FutureIterator($upstream_futures))
-        ->resolveAll();
-    }
-
-    $results = array();
-    foreach ($log_futures as $lib => $future) {
-      list($err, $stdout) = $future->resolve();
-      if (!$err) {
-        list($hash, $epoch) = explode(' ', $stdout);
-        $version = pht('%s (%s)', $hash, phabricator_date($epoch, $viewer));
-      } else {
-        $version = pht('Unknown');
-      }
-
-      $result = array(
-        'version' => $version,
-        'upstream' => null,
-        'branchpoint' => null,
-      );
-
-      $upstream_future = idx($upstream_futures, $lib);
-      if ($upstream_future) {
-        list($err, $stdout) = $upstream_future->resolve();
-        if (!$err) {
-          $branchpoint = trim($stdout);
-          if (strlen($branchpoint)) {
-            // We only list a branchpoint if it differs from HEAD.
-            if ($branchpoint != $hash) {
-              $result['upstream'] = $lib_upstreams[$lib];
-              $result['branchpoint'] = trim($stdout);
-            }
-          }
-        }
-      }
-
-      $results[$lib] = $result;
     }
 
     return $results;
